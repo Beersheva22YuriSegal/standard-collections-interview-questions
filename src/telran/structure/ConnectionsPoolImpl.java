0d2484766b156a5744825e49728c9efd6004705f
YuriSegal <yurisegal8@gmail.com>
2023-03-05 14:50:28 +0200

    HW#35 ConnectionsPool updated to class version

--- src/telran/structure/ConnectionsPoolImpl.java
+++ src/telran/structure/ConnectionsPoolImpl.java
@@ -1,39 +1,98 @@
 package telran.structure;
 
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.*;
 
-public class ConnectionsPoolImpl implements ConnectionsPool {
+public class ConnectionsPoolImpl implements ConnectionsPool{
+	public static class Node {
+		
+		public Connection connection;
+		Node next;
+		Node prev;
+		 Node(Connection connection) {
+		
+			this.connection = connection;
+		}
+	}
+	public static class ListConnections {
+		public Node head;
+		public Node tail;
+		 void addHead(Node nodeConnection) {
+			nodeConnection.next = head;
+			if (head != null) {
+				head.prev = nodeConnection;
+			} else {
+				tail = nodeConnection;
+			}
+			head = nodeConnection;
+			
+		}
+		void removeTail() {
+			tail = tail.prev;
+			if (tail != null) {
+				tail.next = null;
+			}
+			
+		}
+		public void removeNode(Node nodeConnection) {
+			if (nodeConnection == tail) {
+				removeTail();
+			} else {
+				Node before = nodeConnection.prev;
+				Node after = nodeConnection.next;
+				before.next = after;
+				after.prev = before;
+			}
+			
+		}
+	}
+	HashMap<Integer, Node> connectionsMap = new HashMap<>();
+	int connectionsPoolLimit;
+	public ListConnections listConnections = new ListConnections();
 
-	private HashMap<Integer, Object> map = new HashMap<>();
-	private LinkedList<Integer> list = new LinkedList<>();
-	private int listMaxSize = 5;
-	
+	public ConnectionsPoolImpl(int connectionsPoolLimit) {
+		this.connectionsPoolLimit = connectionsPoolLimit;
+	}
+
 	@Override
 	public boolean addConnection(Connection connection) {
-		boolean res = false;
-		int id = connection.getId();
-		if (!map.containsKey(id)) {
-			if (map.size() >= listMaxSize) {
-				map.remove(list.remove());
-			}
-			list.addLast(id);
-			map.put(id, connection);
-			res = true;
+		if (connectionsMap.containsKey(connection.getId())) {
+			return false;
 		}
-		return res;
+		Node nodeConnection = new Node(connection);
+		addMapList(connection, nodeConnection);
+		if (connectionsMap.size() > connectionsPoolLimit) {
+			removeListTailMap();
+		}
+		
+		return true;
 	}
 
+	private void removeListTailMap() {
+		connectionsMap.remove(listConnections.tail.connection.getId());
+		listConnections.removeTail();
+	}
 
-	@SuppressWarnings("unlikely-arg-type")
+	private void addMapList(Connection connection, Node nodeConnection) {
+		connectionsMap.put(connection.getId(), nodeConnection);
+		listConnections.addHead(nodeConnection);
+	}
+
 	@Override
 	public Connection getConnection(int id) {
-		Connection res = (Connection) map.get(id);
-			list.remove(res);
-			list.addLast(id);
-		return res == null ? null : res;
-		
+		Node nodeConnection = connectionsMap.get(id);
+		if (nodeConnection == null) {
+			return null;
+		}
+		moveConnectionToHead(nodeConnection);
+		return nodeConnection.connection;
 	}
 
+	private void moveConnectionToHead(Node nodeConnection) {
+		if (nodeConnection != listConnections.head) {
+		listConnections.removeNode(nodeConnection);
+		listConnections.addHead(nodeConnection);
+		}
+		
+	}
 
 }
--- src/telran/test/ConnectionsPoolTest.java
+++ src/telran/test/ConnectionsPoolTest.java
@@ -1,6 +1,5 @@
 package telran.test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -10,35 +9,62 @@ import telran.structure.Connection;
 import telran.structure.ConnectionsPoolImpl;
 
 class ConnectionsPoolTest {
-	
+
+	private static int CONNECTIONS_POOL_LIMIT = 5;
 	ConnectionsPoolImpl pool;
 
 	@BeforeEach
 	void setUp() {
-		pool = new ConnectionsPoolImpl();
-		pool.addConnection(new Connection(3, "aa", 1));
-		pool.addConnection(new Connection(6, "bb", 2));
-		pool.addConnection(new Connection(9, "cc", 3));
-		pool.addConnection(new Connection(12, "dd", 4));
-		pool.addConnection(new Connection(15, "", 5));
+		pool = new ConnectionsPoolImpl(CONNECTIONS_POOL_LIMIT);
+		pool.addConnection(new Connection(11, "aaa", 1));
+		pool.addConnection(new Connection(22, "bbb", 2));
+		pool.addConnection(new Connection(33, "ccc", 3));
+		pool.addConnection(new Connection(44, "ddd", 4));
+		pool.addConnection(new Connection(55, "eee", 5));
 	}
 
 	@Test
-	void connectionsPoolTest() {
-		assertFalse(pool.addConnection(new Connection(3, "aa", 1)));
-		assertTrue(pool.addConnection(new Connection(18, "ee", 6)));
-		assertFalse(pool.addConnection(new Connection(18, "ee", 6)));
-		assertNull(pool.getConnection(3));
-		assertTrue(pool.addConnection(new Connection(3, "ee", 1)));
-		assertNotNull(pool.getConnection(3));
-		assertFalse(pool.addConnection(new Connection(15, "", 5)));
+	void addConnection() {
+		// trying to add an existing element
+		assertFalse(pool.addConnection(new Connection(33, "ccc", 3)));
+
+		// checked the first element
+		assertEquals(pool.listConnections.head.connection, pool.getConnection(55));
+
+		// added element to full pool
+		assertTrue(pool.addConnection(new Connection(66, "fff", 6)));
+
+		// checked the first element
+		assertEquals(pool.listConnections.head.connection, pool.getConnection(66));
+
+		// checked the last element
+		assertEquals(pool.listConnections.tail.connection, pool.getConnection(22));
+
+		// checked that this element was removed from the pool and it can be added again
+		assertTrue(pool.addConnection(new Connection(11, "aaa", 1)));
+
 	}
-	
+
 	@Test
-	void getConnectionTest() {
-		assertEquals(15, pool.getConnection(15).id);
-		assertEquals("bb", pool.getConnection(6).ipAddress);
-		assertEquals(4, pool.getConnection(12).port);
-		assertNull(pool.getConnection(21));
+	void getConnection() {
+		// take the last element and check that it is the first
+		Connection con55 = pool.getConnection(55);
+		assertEquals(pool.listConnections.head.connection, con55);
+
+		// take the penultimate element and check that it became the last
+		Connection con44 = pool.getConnection(44);
+		assertEquals(pool.listConnections.head.connection, con44);
+
+		assertEquals(33, pool.getConnection(33).id);
+		assertEquals(pool.listConnections.head.connection, pool.getConnection(33));
+
+		assertEquals("bbb", pool.getConnection(22).ipAddress);
+		assertEquals(2, pool.getConnection(22).port);
+
+		// take a non-existent element
+		assertNull(pool.getConnection(99));
+
 	}
+
+
 }
